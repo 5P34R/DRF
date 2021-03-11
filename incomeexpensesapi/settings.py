d24243720d05856ccd1280eac853218aaebc3614
VishDroid-dev <vk2345732@gmail.com>
2021-03-11 11:58:42 +0530

    added smtp service and ui

--- incomeexpensesapi/settings.py
+++ incomeexpensesapi/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
 
     'authentication',
 ]
@@ -129,5 +130,5 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'dreamtester100@gmail.com'
+EMAIL_HOST_PASSWORD = 'DreamTester@123'
